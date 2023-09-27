60d29d3296da9e26cf2ae4e8abcbe995ff5a6960
DêNeves <denilsonazevedoan@gmail.com>
2023-09-27 06:45:57 -0300

    01

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,22 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO:Conhecer e importar a classe scanner
+        
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+        Scanner scanner = new Scanner (System.in).useLocale(Locale.US);
+
+        System.out.println(" Digite seu nome completo ");
+        String nome = scanner.next();
+        }
+
+
     }
-}
